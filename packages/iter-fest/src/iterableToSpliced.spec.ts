715db71b155ab3b40d494c79fc1e2c1f1b718087
William Wong <compulim@hotmail.com>
2024-05-24 13:39:44 -0700

    Should throw with negative number

--- packages/iter-fest/src/iterableToSpliced.spec.ts
+++ packages/iter-fest/src/iterableToSpliced.spec.ts
@@ -6,6 +6,7 @@ describe.each([
   [[1, 2, 3, 4, 5], undefined, undefined, [9]],
   [[1, 2, 3, 4, 5], 1, 1, [9]],
   [[1, 2, 3, 4, 5], 1, 3, [9]],
+  [[1, 2, 3, 4, 5], 1, -1, [9]],
   [[1, 2, 3, 4, 5], 1, 100, [9]],
   [[1, 2, 3, 4, 5], 100, 1, [9]],
   [[1, 2, 3, 4, 5], 1, Infinity, [9]],
@@ -29,3 +30,6 @@ describe.each([
     test('should return same result', () => expect(iterableResult).toEqual(arrayResult));
   }
 );
+
+test('when passing start of -1 should throw TypeError', () =>
+  expect(() => Array.from(iterableToSpliced([], -1, 0))).toThrow('start cannot be a negative finite number'));
