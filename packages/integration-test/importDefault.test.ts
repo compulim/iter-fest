c2a68a16cb9417b31e5c1778935212d792f24df7
William Wong <compulim@hotmail.com>
2024-06-15 18:12:58 -0700

    Remove unneeded tests

--- packages/integration-test/importDefault.test.ts
+++
@@ -1,7 +0,0 @@
-import { SymbolObservable, readerValues } from 'iter-fest';
-
-test('SymbolObservable should work', () => {
-  const observable = new Observable(() => {});
-
-  expect(observable[SymbolObservable]()).toBe(observable);
-});
--- packages/integration-test/requireNamed.test.cjs
+++
@@ -1,8 +0,0 @@
-const { readerValues } = require('iter-fest/readerValues');
-const { SymbolObservable } = require('iter-fest/symbolObservable');
-
-test('SymbolObservable should work', () => {
-  const observable = new Observable(() => {});
-
-  expect(observable[SymbolObservable]()).toBe(observable);
-});
