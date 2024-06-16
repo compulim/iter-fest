44269d6de5035e7b6b59f446139119d740f7b13c
William Wong <compulim@hotmail.com>
2024-06-15 18:39:44 -0700

    Clean up signature

--- packages/iter-fest/src/iteratorToArray.ts
+++ packages/iter-fest/src/iteratorToArray.ts
@@ -1,17 +1,13 @@
 // @ts-expect-error core-js-pure is not typed.
 import toArray from 'core-js-pure/full/iterator/to-array';
 
-type IteratorLike<T> = Pick<Iterator<T>, 'next'>;
-
 /**
  * When you have a non-infinite iterator which you wish to transform into an array, you can do so with the builtin toArray method.
- *
- * @param limit
  *
  * @return Returns an Array containing the values from the iterator.
  *
  * @link https://github.com/tc39/proposal-iterator-helpers/blob/main/README.md
  */
-export function iteratorToArray<T>(iteratorLike: IteratorLike<T>): T[] {
+export function iteratorToArray<T>(iteratorLike: Pick<Iterator<T>, 'next'>): T[] {
   return toArray(iteratorLike);
 }
