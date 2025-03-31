8fa447543c5b1eaf64b4435b36d6171997ed6878
William Wong <compulim@hotmail.com>
2025-03-30 23:26:40 -0700

    Add entrypoint

---
+++ packages/integration-test/test/importDefault/readableStreamValuesWithSignal.mjs
@@ -0,0 +1,15 @@
+import { expect } from 'expect';
+import { readableStreamValuesWithSignal } from 'iter-fest';
+
+describe('readableStreamValuesWithAbort', () => {
+  it('should work with AbortSignal', async () => {
+    const abortController = new AbortController();
+    const readableStream = new ReadableStream();
+
+    const values = readableStreamValuesWithSignal(readableStream, { signal: abortController.signal });
+
+    abortController.abort();
+
+    await expect(values.next()).rejects.toThrow(new DOMException('The operation is aborted', 'AbortError'));
+  });
+});
