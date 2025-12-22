ad35c884ac8d860d3e46e040d8f8b367df97af1a
William Wong <compulim@users.noreply.github.com>
2025-12-22 01:41:23 +0000

    Fix precommit

--- packages/iter-fest/src/iteratorIncludes.spec.ts
+++ packages/iter-fest/src/iteratorIncludes.spec.ts
@@ -13,20 +13,23 @@ describeEach([
   [[1, 2, 3], 1, Infinity],
   [[1, 2, 3], 1, -Infinity],
   [[], 0, undefined]
-])('when compare to %s.includes(%s, %s)', (array: readonly number[], searchElement: number, fromIndex: number | undefined) => {
-  let iterator: Iterator<number>;
-  let arrayResult: boolean;
-  let iteratorResult: boolean;
+])(
+  'when compare to %s.includes(%s, %s)',
+  (array: readonly number[], searchElement: number, fromIndex: number | undefined) => {
+    let iterator: Iterator<number>;
+    let arrayResult: boolean;
+    let iteratorResult: boolean;
 
-  beforeEach(() => {
-    iterator = array.values();
+    beforeEach(() => {
+      iterator = array.values();
 
-    arrayResult = array.includes(searchElement, fromIndex);
-    iteratorResult = iteratorIncludes(iterator, searchElement, fromIndex);
-  });
+      arrayResult = array.includes(searchElement, fromIndex);
+      iteratorResult = iteratorIncludes(iterator, searchElement, fromIndex);
+    });
 
-  test('should return same result', () => expect(iteratorResult).toBe(arrayResult));
-});
+    test('should return same result', () => expect(iteratorResult).toBe(arrayResult));
+  }
+);
 
 test('when passing fromIndex of -1 should throw TypeError', () =>
   expect(() => iteratorIncludes([].values(), 0, -1)).toThrow('fromIndex cannot be a negative finite number'));
--- packages/iter-fest/src/private/describeEach.ts
+++ packages/iter-fest/src/private/describeEach.ts
@@ -1,7 +1,7 @@
 import { describe } from 'node:test';
 import { format } from 'node:util';
 
-export function describeEach<const T extends any[]>(cases: readonly T[]) {
+export function describeEach<const T extends unknown[]>(cases: readonly T[]) {
   return (name: string, fn: (...args: T) => void) => {
     for (const args of cases) {
       describe(format(name, ...args), () => {
